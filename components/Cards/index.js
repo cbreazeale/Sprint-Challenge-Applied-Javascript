a896f8518c43f1c0df81a01b7b5d23d1b06f9326
clay2 <clay@DESKTOP-B145MCP.localdomain>
2022-09-02 14:51:06 -0500

    finished MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,51 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+
+const url2 = 'https://lambda-times-backend.herokuapp.com/articles'
+axios(url2)
+.then(res => {
+    const articlesObj = res.data.articles;
+    // console.log(articlesObj)
+    for (const article in articlesObj){
+        // console.log(articlesObj[article])
+        const articleContent = articlesObj[article]
+
+        document.querySelector('.cards-container').appendChild(cardCreator(articleContent))
+    }
+console.log(document.querySelector('.cards-container'))
+})
+
+
+function cardCreator(data){
+
+    const div1 = document.createElement('div')
+    const div2 = document.createElement('div')
+    const div3 = document.createElement('div')
+    const div4 = document.createElement('div')
+    const img = document.createElement('img')
+    const span = document.createElement('span')
+
+    div1.classList.add('card')
+    div2.classList.add('headline')
+    div3.classList.add('author')
+    div4.classList.add('img-container')
+
+    // div2.textContent =
+
+    data.forEach(element => {
+        const vals = Object.values(element)
+        // console.log(Object.keys(element))
+        div2.textContent = vals[0]
+        img.src = vals[1]
+        span.textContent = 'By ' + vals[2]
+    });
+
+    div1.append(div2,div3)
+    div3.append(div4,span)
+    div4.appendChild(img)
+
+    console.log(div1)
+    return div1
+}
