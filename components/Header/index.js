91b9b6c60240a55241f526c90cf6badd55616d8c
clay2 <clay@DESKTOP-B145MCP.localdomain>
2022-09-02 13:20:40 -0500

    completed header and topics

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,23 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const div = document.createElement('div')
+    const span1 = document.createElement('span')
+    const h1 = document.createElement('h1')
+    const span2 = document.createElement('span')
+
+    div.classList.add('header')
+    span1.classList.add('date')
+    span2.classList.add('temp')
+
+    span1.textContent = 'SMARCH 28, 2019'
+    h1.textContent = 'Lambda Times'
+    span2.textContent = '98°'
+
+    div.append(span1, h1, span2)
+    return div
+}
+
+const header = document.querySelector('div.header-container')
+header.appendChild(Header())
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,25 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+const url = 'https://lambda-times-backend.herokuapp.com/topics'
+
+axios(url)
+.then(res => {
+    const topics = res.data.topics
+        topics.forEach(element => {
+        document.querySelector('div.topics').appendChild(createTopics(element))
+    });
+})
+
+function createTopics(data) {
+    const newArr = data
+
+    const topic = document.createElement('div')
+    topic.classList.add('tab')
+    topic.textContent = newArr
+
+    return topic
+}
